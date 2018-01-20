cb22c1814616ec6582679370e640f5f4f54bd99f
RACHELS MAC <rachelsmac@RACHELSs-MacBook-Air.local>
2018-01-20 16:37:47 +0000

    nav component

---
+++ src/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Nav from './../components/Nav'
+
+
+export default class Home extends Component {
+    render(){
+        return (
+            <div>
+                <Nav/>
+            </div>
+        )
+    }
+}
