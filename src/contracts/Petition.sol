5f32b2d924448c873ed1ac4de09b8641f5cb65ad
Harry Wright <hcawdev@gmail.com>
2018-01-20 16:06:45 +0000

    Work on Petition contract.

--- src/contracts/Petition.sol
+++ src/contracts/Petition.sol
@@ -1,42 +1,41 @@
 pragma solidity ^0.4.18;
 
-import "./IdManage.sol";
+import "./IdManager.sol";
 
-contract PetitionManager {
+contract Petition {
 
-    struct Petition {
-        mapping(address => bool) hasVoted;
-        string description;
-        uint votesFor;
-        uint votesAgainst;
-        uint256 endEpoch;
-    }
+    address idManager;
+    string description;
+    uint256 endEpoch;
 
-    Petition[] allPetitions;
-    address idManager;    
+    mapping(address => bool) hasVoted;
 
-    function PetitionManager(address _idManager) public {
+    // Should beware of overflows here! (maybe use safeMath library...)
+    uint votesFor;
+    uint votesAgainst;
+    
+    uint256 constant VOTE_FOR = 1;
+    uint256 constant VOTE_AGAINST = 0;
+
+    function Petition(address _idManager, string _description, uint256 _endEpoch) public {
         idManager = _idManager;
+        description = _description;
+        endEpoch = _endEpoch;
     }
 
-    function getTenPetitions(uint startIndex) public view returns {
-        
-    }
-
-    function vote(bytes32 vote) {
+    function vote(uint256 _vote) public {
         // check ID is valid
-        require(IdManager(idManager).checkId(msg.sender));
+        require(idManager.delegatecall(bytes4(keccak256("isValidId()"))));
+        // Shouldn't really do this as time can be manipulated by miners!
+        require(now < endEpoch);
+        require(!hasVoted[msg.sender]);
 
-
-
-        if (!hasVoted[msg.sender]) {
-            if (vote == bytes32("for")) {
-                votesFor++;
-            } else if (vote == bytes32("against")) {
-                votesAgainst++;
-            } else {
-                revert();
-            }
+        if (_vote == VOTE_AGAINST) {
+            votesAgainst++;
+        } else if (_vote == VOTE_FOR) {
+            votesFor++;
+        } else {
+            revert();
         }
     }
 }
