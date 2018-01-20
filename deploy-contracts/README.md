25139b06d47232de560e39a7d5b0c613ec263b44
Harry Wright <hcawdev@gmail.com>
2018-01-20 22:22:18 +0000

    Created directory to deploy contracts from and started a python script to try out web3.py.

---
+++ deploy-contracts/README.md
@@ -0,0 +1,1 @@
+Contracts should be deployed from here
---
+++ server/contractStuff.py
@@ -0,0 +1,7 @@
+# A script for testing out web3.py stuff
+
+from web3 import Web3, HTTPProvider
+web3 = Web3(HTTPProvider('http://localhost:9545'))
+
+# Calling the below shows we are able to query the local node
+web3.eth.blockNumber
