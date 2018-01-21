5d75fda0d6669bce2f4373dc2653cd3fb579e285
Harry Wright <hcawdev@gmail.com>
2018-01-21 13:57:20 +0000

    Integrated SafeMath in the Petition contract.

--- src/contracts/Petition.sol
+++ src/contracts/Petition.sol
@@ -17,17 +17,16 @@ contract Petition {
     function Petition(address _idManager, string _description, uint256 _dayLength) public {
         idManager = _idManager;
         description = _description;
-        endEpoch = now + (_dayLength * 1 days);
+        endEpoch = SafeMath.add(now,(SafeMath.mul(_dayLength,1 days)));
     }
 
     function vote() public {
-        // check ID is valid
-        // require(idManager.delegatecall(bytes4(keccak256("isValidId()"))));
+        require(idManager.delegatecall(bytes4(keccak256("isValidId()"))));
         // Shouldn't really do this as time can be manipulated by miners!
-        // require(now < endEpoch);
+        require(now < endEpoch);
         require(!hasVoted[msg.sender]);
         hasVoted[msg.sender] = true;
-        votesFor++;
+        votesFor = SafeMath.add(1,votesFor);
     }
 
     function checkHasVoted() public view returns (bool) {
